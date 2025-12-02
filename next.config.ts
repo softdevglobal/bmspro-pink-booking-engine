2d549795eb8013ef5e7dc242340c22ba26c5f30f
pawaan9 <pawankanchana99@gmail.com>
2025-12-02 13:46:45 +0530

    build

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
-  serverComponentsExternalPackages: ["firebase-admin"],
+  serverExternalPackages: ["firebase-admin"],
 };
 
 export default nextConfig;
